066536572fe50f10b963cd0842a411d3b9cff423
TuringEmmy <yonglonggeng@163.com>
2018-11-11 17:01:38 +0800

    bayes is begining
    Signed-off-by: TuringEmmy <yonglonggeng@163.com>

--- 09 linear_regression/LinearRegression_study.py
+++ 09 linear_regression/LinearRegression_study.py
@@ -5,13 +5,13 @@
 
 from sklearn.datasets import load_boston
 # 波斯顿房价
-from sklearn.linear_model import LinearRegression,SGDRegressor
+from sklearn.linear_model import LinearRegression, SGDRegressor
 
 from sklearn.model_selection import train_test_split
 
-
 # 标准化预处理
 from sklearn.preprocessing import StandardScaler
+
 
 # 预测房子价格
 def myLinear():
@@ -23,27 +23,28 @@ def myLinear():
     # 获取数据
     lb = load_boston()
     # 分割数据集到训练集合测试集
-    x_train,x_test,y_train,y_test = train_test_split(lb.data, lb.target,test_size=0.25)
-    
-    print(y_train,y_test)
+    x_train, x_test, y_train, y_test = train_test_split(lb.data, lb.target, test_size=0.25)
+
+    print(y_train, y_test)
     # 进行标准化处理  (？)目标值是否需要标准化处理？为什么需要？
     # 特征值和目标值都是必须进行标准化处理，要实例化两个保准API
     std_x = StandardScaler()
-    x_train= std_x.fit_transform(x_train)
+    x_train = std_x.fit_transform(x_train)
     x_test = std_x.transform(x_test)
 
     # 目标值
     std_y = StandardScaler()
 
     # *******************0.19版本必须传入二维的数据************************
-    y_train = std_y.fit_transform(y_train。reshape(-1,1))
+    y_train = std_y.fit_transform(y_train.reshape(-1, 1))
+
     y_test = std_y.transform(y_test)
 
     # estimator预测
     # 正规方程求解方式预测结果
     lr = LinearRegression()
 
-    lr.fit(x_train,y_train)
+    lr.fit(x_train, y_train)
 
     print(lr.coef_)
 
